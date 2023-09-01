a0d896ac22e22f26dc59ae6f94ab06836837b9d2
marcosamado <led_9dejulio@hotmail.com>
2023-09-01 17:23:04 -0300

    trabajando en la logica de Pagination

--- src/ELICOMMERCE/components/Main/Pagination.jsx
+++ src/ELICOMMERCE/components/Main/Pagination.jsx
@@ -1,15 +1,42 @@
-import { Link } from "react-router-dom";
+import queryString from "query-string";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Pagination = () => {
+    const [productNumbers, setProductNumbers] = useState(1);
+    const location = useLocation();
+    const { search } = queryString.parse(location.search);
+    const navigate = useNavigate();
+
+    const handleSum = () => {
+        setProductNumbers((prev) => prev + 50);
+        console.log(search);
+        const parts = search.split("_");
+        console.log(parts);
+        const newPart = parts[0];
+        console.log(newPart);
+
+        navigate(`/items?search=${newPart}_Desde_${productNumbers}`);
+    };
+    const handleRest = () => {
+        setProductNumbers((prev) => prev - 50);
+        console.log(search);
+        const parts = search.split("_");
+        console.log(parts);
+        const newPart = parts[0];
+        console.log(newPart);
+
+        navigate(`/items?search=${newPart}_Desde_${productNumbers}`);
+    };
     return (
         <div className="">
             <nav className="flex">
-                <Link to="">
+                <button onClick={handleRest}>
                     <span>
                         <i className="fa-solid fa-chevron-left"></i>
                     </span>
                     <span>Anterior</span>
-                </Link>
+                </button>
                 <li>
                     <span>paginaActual </span>
                     de
@@ -17,12 +44,12 @@ const Pagination = () => {
                 <li>
                     <span> totalDePagina</span>
                 </li>
-                <Link to="">
+                <button onClick={handleSum}>
                     <span>
                         <i className="fa-solid fa-chevron-right"></i>
                     </span>
                     <span>Siguiente</span>
-                </Link>
+                </button>
             </nav>
         </div>
     );
